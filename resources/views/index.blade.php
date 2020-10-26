c2b0fb47a6f63ceeb9529a55a8a123205d6faa4e
Davide Cesare <davidereaper@gmail.com>
2020-10-26 17:12:10 +0100

    Added some content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,18 +1,32 @@
-<table>
-  <thead>
-    <tr>
-      <th>Nome</th>
-      <th>Cognome</th>
-      <th>Sesso</th>
-      <th>Data di nascita</th>
-    </tr>
-  </thead>
-  @foreach ($students as $student)
-    <tr>
-      <td>{{$student->name}}</td>
-      <td>{{$student->lastname}}</td>
-      <td>{{$student->gender}}</td>
-      <td>{{$student->date_of_birth}}</td>
-    </tr>
-  @endforeach
-</table>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  </head>
+  <body>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Cognome</th>
+          <th>Sesso</th>
+          <th>Data di nascita</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($students as $student)
+        <tr>
+          <td>{{$student->name}}</td>
+          <td>{{$student->lastname}}</td>
+          <td>{{$student->gender}}</td>
+          <td>{{$student->date_of_birth}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+
+  </body>
+</html>
